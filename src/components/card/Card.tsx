3fc56e5c233407f040fb2ed5ede91ef0a2bfdb0b
Craig <indigomx9@gmail.com>
2023-01-20 21:43:55 -0500

    Add React Router, created Header, Card, and Footer components.

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+export const Card = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>Card</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet non quos, numquam voluptas blanditiis omnis optio incidunt, sunt ut distinctio porro odit impedit culpa velit asperiores deserunt, dicta labore. Dolore.</p>
+        </React.Fragment>
+    );
+};
+
+
